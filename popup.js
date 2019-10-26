e8b17d9362af15b00b231a874bcacd3f7e52cdff
Sdogaru <sdogaru@ucsd.edu>
2019-10-25 22:14:42 -0700

    Added demo response to setting button

---
+++ popup.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function() {
+  document.getElementById("checkPage").addEventListener("click", myFunction);
+});
+
+function myFunction(){
+  document.getElementById("demo").innerHTML = "fuck";
+}
